1f9bb3457e66b319ab2a021e104a45dd2b4b5b80
TechDrivers <icaro.dellosso@hotmail.com>
2023-03-14 23:19:22 -0300

    mudança na ordem

--- api/index.js
+++ api/index.js
@@ -4,16 +4,16 @@ const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const app = express()
+const port = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
 app.use(cors({
     origin: 'http://localhost:3000', // Quando fizer o deploy do site "origin: ['http://localhost:3000', 'http://meu-outro-app.com']"
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 routes(app)
 
-const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
 
