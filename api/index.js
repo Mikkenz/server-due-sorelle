534ff20ce785df1b7c9443092b89dfd686fee5e4
TechDrivers <icaro.dellosso@hotmail.com>
2023-03-14 23:08:29 -0300

    mudança nas portas

--- api/index.js
+++ api/index.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const app = express()
-const port = 3001
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(cors())
 routes(app)
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
 
